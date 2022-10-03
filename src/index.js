f71b7cdf7cc985d40b912e574e6c3c6dcbaed449
Judy Palacios <palacios.carolina0@gmail.com>
2022-10-03 12:14:48 -0500

    Componente button y vista Waiter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 import { Login } from "./views/Login/Login";
+import { WaiterView } from "./views/Waiter/Waiter";
 
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-    <Login/>
+    <>
+        <Login/>
+        <WaiterView/>
+    </>
 )
 
 
