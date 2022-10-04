7bc933506391ccddd02bad97e5b7964ae48297ec
lxsca00 <ljimena10@gmail.com>
2022-10-03 19:01:57 -0500

    login view

--- burger-queen/src/App.js
+++ burger-queen/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Login from './Login.jsx';
+import Modal from './Modal';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Login/>
+      <Modal>
+          <p> Error desde children </p>
+      </Modal>
     </div>
   );
 }
