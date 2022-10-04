fe987f658b9e60778f03d8df67dd173a0a72c6f7
Judy Palacios <palacios.carolina0@gmail.com>
2022-10-04 16:36:31 -0500

    componente botón actualizado, componente card y vista mesero

--- src/views/Waiter/Waiter.jsx
+++ src/views/Waiter/Waiter.jsx
@@ -1,5 +1,6 @@
-import { MenuButton } from "../../components/Buttons/Button";
 import React from "react";
+import { MenuButton } from "../../components/Buttons/Button";
+import { CardMenu } from "../../components/Card/Card";
 import './Waiter.css';
 import '../../components/Buttons/Button.css';
 
@@ -8,25 +9,25 @@ export const WaiterView = () => {
         <section className="waiter">
             <div className="content-waiter">
                 <div className="container-menu">
-                    <div>
+                    <div className="nav-menu">
                         <MenuButton title='Desayuno'/>
                         <MenuButton title='Almuerzo y Cena'/>
                     </div>
-                    <div>
-                        <p>Tarjeta</p>
-                        <p>Tarjeta</p>
-                        <p>Tarjeta</p>
-                        <p>Tarjeta</p>
+                    <div className="container-card-menu">
+                        <CardMenu/>
+                        <CardMenu/>
+                        <CardMenu/>
+                        <CardMenu/>           
                     </div>
                 </div>
 
-                <div>
-                    <div>
+                <div className="container-menu items-orders">
+                    <div className="nav-menu">
                         <label>Cliente
                             <input type="text"/>
                         </label>
                     </div>
-                    <div>
+                    <div className="container-orders">
                         <p>Item</p>
                         <p>Item</p>
                         <p>Item</p>
