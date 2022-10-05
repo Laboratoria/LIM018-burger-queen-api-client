e3fd199d2c30dc5d2d1189cd822ab6f75058f060
Judy Palacios <palacios.carolina0@gmail.com>
2022-10-05 13:10:15 -0500

    creación de la API falsa

---
+++ src/fakeApi/index.js
@@ -0,0 +1,42 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('./db.json')
+const middlewares = jsonServer.defaults()
+
+// const secret = "EsUnSecreto"
+
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+
+// server.use((req, res, next) => {
+
+//   console.log(req.headers);
+
+
+//   if(req.method === "POST" && req.path === "/auth") {
+//    next();
+//   } else if(req.headers.authorization === `Bearer ${secret}`) {
+//    next()
+//   } else {
+//    res.sendStatus(401)
+//   }
+//  })
+
+
+server.post('/auth', (req, res) => {
+    console.log(req.body);
+
+    res.jsonp(req.query);
+//   if (
+//     req.body.email === 'mesero1@gmail.com' &&
+//     req.body.password === '123456') {
+//     res.jsonp({
+//       token: secret
+//     })
+//   } else res.status(400).send('Bad Request')
+})
+
+server.use(router)
+server.listen(3001, () => {
+  console.log('JSON Server is running')
+})
