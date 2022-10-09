a1c4a26fb31c0f48aea9a46c2228b9a3867fdb06
@StefaniPenaranda <pstefanipm@gmail.com>
2022-10-09 16:13:27 -0500

    app component angular

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NgIfContext } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+
+  product : Product = {
+    id:'1',
+    name: "cafe con leche",
+    price: 10,
+    img: './assets/icons/icon.png'
+  }
+
+
+
+    {
+      id:'2',
+      name: "cafe americano",
+      price: 20,
+      img: 'https://www.splenda.com/wp-content/themes/bistrotheme/assets/recipe-images/cafe-con-leche.jpg'
+    }
+
+  // newName = '';
+  // toggleButton(){
+  //   this.btndisable = !this.btndisable;
+  // }
+  // ageChange(){
+  //   this.person.age += 1
+  // }
+  // // changeName(event:Event){
+  // //   const elemet = event.target as HTMLInputElement;
+  // //   this.person.name = elemet.value;
+  // // }
+  // deleteName(index: number){
+  //   this.names.splice(index,1)
+  // }
+  // addName(){
+  //   this.names.push(this.newName);
+  //   this.newName = '';
+  // }
 }
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
