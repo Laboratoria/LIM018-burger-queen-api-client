a3ff611d73a43852cd77139a61f5d1697f80ad43
@StefaniPenaranda <pstefanipm@gmail.com>
2022-10-20 11:34:55 -0500

    token interceptor

--- my-app/src/app/components/login/login.component.ts
+++ my-app/src/app/components/login/login.component.ts
@@ -22,12 +22,40 @@ export class LoginComponent {
     this.authService.login(this.email, this.password)
     .subscribe(rta => {
       console.log(rta.token);
-      this.token = rta.token;
-      // localStorage.setItem("token", rta.token)
-      // console.log('load hijo');
-      // this.loaded.emit(this.token);
-
       this.router.navigate(['/home'])
     });
   }
 }
+// import { Component} from '@angular/core';
+// import { AuthService } from '../../services/auth.service';
+// import { Router } from '@angular/router';
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.scss']
+// })
+// export class LoginComponent {
+//   token = ''
+//   email = 'iam@fakel.lol'
+//   password = 'apasswordtochange'
+
+// // tenemos que injectar en el contructor el servicio router
+//   constructor(
+//     private authService: AuthService,
+//     private router: Router
+//   ) { }
+
+//   login() {
+//     this.authService.login(this.email, this.password)
+//     .subscribe(rta => {
+//       console.log(rta.token);
+//       this.token = rta.token;
+//       // localStorage.setItem("token", rta.token)
+//       // console.log('load hijo');
+//       // this.loaded.emit(this.token);
+
+//       this.router.navigate(['/home'])
+//     });
+//   }
+// }
