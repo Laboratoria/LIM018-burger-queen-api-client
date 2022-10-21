e1725e5f21d8390b39b80a185bd114648c4682f5
belen <anabelenarroyo21@gmail.com>
2022-10-21 08:46:29 -0500

    modificado filtrado breakfast

--- my-app/src/app/components/Home/home.component.ts
+++ my-app/src/app/components/Home/home.component.ts
@@ -1,85 +1,78 @@
 import { ExpressionType } from '@angular/compiler';
 import { Component, OnInit, Type } from '@angular/core';
-import {Product} from '../../models/products.model';
+import { Product } from '../../models/products.model';
 import { ProductsService } from '../../services/products.service';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 
 // export class homeComponent{
 //   name : "Stefani";
 // }
 export class homeComponent implements OnInit {
-  name = "Stefani";
-  myorder: Product[] = []; // se pone privado para proteger la accecibilidad
+  name = 'Stefani';
+  myOrder: Product[] = []; // se pone privado para proteger la accecibilidad
   total = 0;
-  product: Product[] =[];
-  filterProduct: Product[] =[]
-  //
-  token = '';
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  token: String = '';
 
- // para mostrar y ocultar con el boton
-  mostrar : Boolean = false;
+  // para mostrar y ocultar con el boton
+  mostrar: Boolean = false;
 
-  mostrarOcultar(){
-    if(this.mostrar){
-      this.mostrar= false;
-    }else {
-      this.mostrar= true;
+  mostrarOcultar() {
+    if (this.mostrar) {
+      this.mostrar = false;
+    } else {
+      this.mostrar = true;
     }
   }
 
-  constructor(
-    private productsService : ProductsService,
-    ) {
-    this.myorder = this.productsService.getmyOrder();
+  constructor(private productsService: ProductsService) {
+    this.myOrder = this.productsService.getmyOrder();
   }
 
-//para manejar peticiones asincronas
+  //para manejar peticiones asincronas
   ngOnInit(): void {
-
-    this.productsService.getAllProducts()
-    .subscribe(data=>{
-      console.log(data[0].type)
-      this.product=  data
-    })
-  }
-// para filtrar por type breakfast
-  viewBreakfast = () =>{
-    this.productsService.getAllProducts()
-    .subscribe(data => {
-      console.log(data)
-      this.filterProduct= data.filter( e =>{
-        console.log(e)
-        const breakfast = e.type === "breakfast"
-        console.log(breakfast)
-      });
+    this.productsService.getAllProducts().subscribe((data) => {
+      console.log(data[0].type);
+      this.products = data;
     });
   }
-// para ver por cena
-viewLunch = () => {
-  this.productsService.getAllProducts()
-  .subscribe(data =>{
-    this.filterProduct = data.filter( e =>{
-      const prueba = e.type == "lunch";
-    })
-  })
-}
 
-//para agregar los productos y contabilizarlos y calcular el totoal del precio
-  onAddToOrder(product: Product){
+  // para filtrar por type breakfast
+  viewBreakfast = () => {
+    this.productsService.getAllProducts().subscribe((data) => {
+      console.log(data);
+      this.filteredProducts = data.filter((element) => {
+        return element.type === 'breakfast';
+        //console.log(breakfast);
+      });
+
+      console.log('filterProduct', this.filteredProducts);
+    });
+  };
+  // para ver por cena
+  viewLunch = () => {
+    this.productsService.getAllProducts().subscribe((data) => {
+      this.filteredProducts = data.filter((e) => {
+        const prueba = e.type == 'lunch';
+      });
+    });
+  };
+
+  //para agregar los productos y contabilizarlos y calcular el totoal del precio
+  onAddToOrder(product: Product) {
     this.productsService.addProduct(product);
     this.total = this.productsService.getTotal();
   }
-// para poder cambiar el nombreee
-  changeName(event:Event){
-    console.log(event)
-    const element = event.target as HTMLInputElement
-    this.name = element.value
+  // para poder cambiar el nombreee
+  changeName(event: Event) {
+    console.log(event);
+    const element = event.target as HTMLInputElement;
+    this.name = element.value;
   }
-
 }
